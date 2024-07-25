1ffc6ae467716af336a0b17ccda1378c2277a62b
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-25 08:57:53 -0500

    Added loader in react router

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,30 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+// import Loader from "./Loader";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  console.log(navigation);
   return (
-    <div>
-      <Header />
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
-      <CartOverview />
+    <div className="layout">
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <Header />
+            <main>
+              <h1>Content</h1>
+              <Outlet />
+            </main>
+            <CartOverview />
+          </>
+        )}
+      </>
     </div>
   );
 }
