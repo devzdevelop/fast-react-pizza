2dc31065421535b14f0836cb33263e3ad2f18119
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-28 20:09:08 -0500

    Styled the components.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
   const [query, setQuery] = useState();
@@ -8,7 +8,7 @@ function SearchOrder() {
   function handleSubmit(e) {
     e.preventDefault();
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
@@ -17,6 +17,7 @@ function SearchOrder() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search order #"
+        className="placeholder:text-stone-00 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,30 +1,24 @@
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
 // import Loader from "./Loader";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 export default function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   // console.log(navigation);
   return (
-    <div className="layout">
-      <>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            <Header />
-            <main>
-              <h1>Content</h1>
-              <Outlet />
-            </main>
-            <CartOverview />
-          </>
-        )}
-      </>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
     </div>
   );
 }
