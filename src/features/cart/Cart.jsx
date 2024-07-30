ed73f4863682c9a6cab09b66a368672c467566af
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-30 08:12:51 -0500

    Cart is working along withe modifying the quantity.

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -5,7 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart } from './cartSlice';
 import { getUser } from '../user/userSlice';
 import EmptyCart from './EmptyCart';
-
 function Cart() {
   const cart = useSelector(getCart);
   const username = useSelector(getUser);
@@ -16,12 +15,11 @@ function Cart() {
   return (
     <div className='px-4 py-3'>
       <LinkButton to='/menu'>&larr; Back to menu</LinkButton>
-
       <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
 
       <ul className='mt-3 divide-y divide-stone-200 border-b'>
         {cart.map(item => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
