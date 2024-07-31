f75e4b7bef26031467272960b8ae74b9a43f8f12
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-31 05:34:20 -0500

    Added Order functionality.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,11 @@
+//ORDER ID W23D5C
 import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { getCart } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = str =>
@@ -10,33 +13,10 @@ const isValidPhone = str =>
     str
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  console.log(cart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
@@ -44,6 +24,8 @@ function CreateOrder() {
   console.log(formErrors);
 
   const username = useSelector(state => state.user.username);
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className='px-4 py-6'>
